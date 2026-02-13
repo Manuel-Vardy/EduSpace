501295eb3036d5debf342e2ba641cb0e3ae82089
Manuel Vardy <manuelvardy83@gmail.com>
2026-02-13 06:34:38 +0000

    cards updated

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 about: resolve(__dirname, 'about.html'),
-                contact: resolve(__dirname, 'contact.html'),
                 products: resolve(__dirname, 'products.html'),
                 news: resolve(__dirname, 'news.html'),
                 services: resolve(__dirname, 'services.html'),
